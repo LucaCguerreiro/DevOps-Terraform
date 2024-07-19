a3e57ca26ec19fc554e13d7405bbb25b4d6cdb95
Luca Capeloci <lucacguerreiro@hotmail.com>
2024-07-19 15:31:52 -0300

    Refactor: Terraform modules

--- modules/network/ngw.tf
+++ modules/network/ngw.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "eks_ngw_eip_1a" {
   domain = "vpc"
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-EIP-1a"
     }
@@ -12,7 +12,7 @@ resource "aws_eip" "eks_ngw_eip_1a" {
 resource "aws_eip" "eks_ngw_eip_1b" {
   domain = "vpc"
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-EIP-1b"
     }
@@ -25,7 +25,7 @@ resource "aws_nat_gateway" "eks_ngw_1a" {
   subnet_id     = aws_subnet.devops_subnet_public_1a.id
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-ngw-1a"
     }
@@ -38,7 +38,7 @@ resource "aws_nat_gateway" "eks_ngw_1b" {
   subnet_id     = aws_subnet.devops_subnet_public_1b.id
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-ngw-1b"
     }
@@ -55,7 +55,7 @@ resource "aws_route_table" "eks_private_route_table_1a" {
   }
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-private-route-table-1a"
     }
@@ -72,7 +72,7 @@ resource "aws_route_table" "eks_private_route_table_1b" {
   }
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-private-route-table-1b"
     }
--- modules/network/private.tf
+++ modules/network/private.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "devops_subnet_private_1a" {
   availability_zone = "${data.aws_region.current.name}a"
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                              = "${var.project_name}-private-subnet-1a"
       "kubernetes.io/role/internal-elb" = 1
@@ -19,7 +19,7 @@ resource "aws_subnet" "devops_subnet_private_1b" {
   availability_zone = "${data.aws_region.current.name}b"
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                              = "${var.project_name}-private-subnet-1b"
       "kubernetes.io/role/internal-elb" = 1
