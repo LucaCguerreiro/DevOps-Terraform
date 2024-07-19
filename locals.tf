89159ea159bf999c7cbb70b02bc192e108dea15d
Luca Capeloci <lucacguerreiro@hotmail.com>
2024-07-19 11:00:02 -0300

    Feat: decouple tags, cidrsubnet function

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  tags = {
+    Departament  = "DevOps"
+    Organizarion = "Infraestructure & Operations"
+    Project      = "DevOps-Terraform"
+    Enviromment  = "Development"
+  }
+}
--- public.tf
+++ public.tf
@@ -1,21 +1,32 @@
 resource "aws_subnet" "devops_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "devops-terraform-subnet-1a"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "devops-terraform-subnet-1a"
+      "kubernetes.io/role/elb" = 1
+    }
+
+  )
 }
+
 
 resource "aws_subnet" "devops_subnet_public_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "devops-terraform-subnet-1b"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "devops-terraform-subnet-1b"
+      "kubernetes.io/role/elb" = 1
+    }
+
+  )
 }
