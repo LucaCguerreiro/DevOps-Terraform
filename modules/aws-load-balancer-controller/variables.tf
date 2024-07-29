814dc6262e3781f65ef11ea886cdac11bbab96c9
Luca Capeloci <lucacguerreiro@hotmail.com>
2024-07-29 16:56:28 -0300

    Feature: added load-ballancer-controller with helm

--- modules/aws-load-balancer-controller/variables.tf
+++ modules/aws-load-balancer-controller/variables.tf
@@ -7,3 +7,13 @@ variable "tags" {
   type        = map(any)
   description = "Tags to be added to aws resources"
 }
+
+variable "oidc" {
+  type        = string
+  description = "HTTPS URL from OIDC provider"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster Name"
+}
